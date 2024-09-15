44706dbb674e9a422a63cd78b73479e30dde88de
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-09-15 19:34:45 +0300

    added a custom DSL for printing news

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,26 @@
 package ru.tbank
 
-fun main() {
-    println("Hello World!")
+import ru.tbank.dsl.newsPrinter
+import ru.tbank.service.NewsService
+import ru.tbank.utils.getMostRatedNews
+import java.time.LocalDate
+
+suspend fun main() {
+    val newsService = NewsService()
+
+    val newsList = newsService.getNews(100)
+    val mostRatedNews = newsList.getMostRatedNews(10, LocalDate.of(2024, 1, 1)..LocalDate.of(2024, 12, 31))
+    newsService.saveNews("src/main/resources/news/news.csv", mostRatedNews)
+
+    val output = newsPrinter {
+        header(level = 1) { append("Most Rated News") }
+
+        mostRatedNews.forEach { news ->
+            news(news)
+        }
+    }
+
+    println(output.build())
+
+    output.saveToFile("src/main/resources/news/news.md")
 }
