a1a38fffb9beebc216419deae3a98355ad2fc738
vyacheslavnov03 <vyacheslavnov03@mail.ru>
2024-10-20 20:52:05 +0300

    added test

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,12 @@ dependencies {
 
     implementation("org.slf4j:slf4j-api:1.7.36")
     implementation("ch.qos.logback:logback-classic:1.2.11")
+
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
+    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
 }
 
 tasks.test {
